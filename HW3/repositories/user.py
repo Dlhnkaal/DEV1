0c0a8de0e65d8890476a5ad5445116e1c0dc94ca
Dlhnkaal <74089479+Dlhnkaal@users.noreply.github.com>
2026-02-09 12:58:59 +0300

    Commit 8

--- HW3/repositories/user.py
+++ HW3/repositories/user.py
@@ -1,68 +1,89 @@
+import logging
 from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Mapping, Any, Sequence
+
 from clients.postgres import get_pg_connection
 from errors import UserNotFoundError
 from models.user import UserCreate, UserInDB
 
-@dataclass
-class UserRepository:
-    
-    async def create(self, user_data: UserCreate) -> UserInDB:
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class UserPostgresStorage:
+    async def create(self, name: str, email: str, is_verified_seller: bool) -> Mapping[str, Any]:
+        logger.info("Creating user name=%s, email=%s", name, email)
         query = """
             INSERT INTO users (name, email, is_verified_seller)
             VALUES ($1, $2, $3)
             RETURNING id, name, email, is_verified_seller, created_at
         """
-        
         async with get_pg_connection() as connection:
             row = await connection.fetchrow(
-                query,
-                user_data.name,
-                user_data.email,
-                user_data.is_verified_seller)
-            
-            if not row:
-                raise Exception("Failed to create user")
-            
-            return UserInDB(**dict(row))
-    
-    async def get_by_id(self, user_id: int) -> UserInDB:
+                query, name, email, is_verified_seller)
+            if row:
+                return dict(row)
+            raise Exception("Failed to create user")
+
+    async def get_by_id(self, user_id: int) -> Mapping[str, Any]:
+        logger.info("Selecting user by id=%s", user_id)
         query = """
             SELECT id, name, email, is_verified_seller, created_at
             FROM users
             WHERE id = $1
         """
-        
         async with get_pg_connection() as connection:
             row = await connection.fetchrow(query, user_id)
-            
-            if not row:
-                raise UserNotFoundError(f"User with id {user_id} not found")
-            
-            return UserInDB(**dict(row))
-    
-    async def get_by_email(self, email: str) -> Optional[UserInDB]:
+            if row:
+                return dict(row)
+            raise UserNotFoundError(f"User with id {user_id} not found")
+
+    async def get_by_email(self, email: str) -> Optional[Mapping[str, Any]]:
+        logger.info("Selecting user by email=%s", email)
         query = """
             SELECT id, name, email, is_verified_seller, created_at
             FROM users
             WHERE email = $1
         """
-        
         async with get_pg_connection() as connection:
             row = await connection.fetchrow(query, email)
-            
-            if not row:
-                return None
-            
-            return UserInDB(**dict(row))
-    
-    async def get_all(self) -> List[UserInDB]:
+            if row:
+                return dict(row)
+            return None
+
+    async def get_all(self) -> Sequence[Mapping[str, Any]]:
+        logger.info("Selecting all users")
         query = """
             SELECT id, name, email, is_verified_seller, created_at
             FROM users
             ORDER BY created_at DESC
         """
-        
         async with get_pg_connection() as connection:
             rows = await connection.fetch(query)
-            return [UserInDB(**dict(row)) for row in rows]
+            return [dict(row) for row in rows]
+
+
+@dataclass
+class UserRepository:
+    storage: UserPostgresStorage = UserPostgresStorage()
+
+    async def create(self, user_data: UserCreate) -> UserInDB:
+        raw_user = await self.storage.create(
+            name=user_data.name,
+            email=user_data.email,
+            is_verified_seller=user_data.is_verified_seller)
+        return UserInDB(**raw_user)
+
+    async def get_by_id(self, user_id: int) -> UserInDB:
+        raw_user = await self.storage.get_by_id(user_id)
+        return UserInDB(**raw_user)
+
+    async def get_by_email(self, email: str) -> Optional[UserInDB]:
+        raw_user = await self.storage.get_by_email(email)
+        if raw_user:
+            return UserInDB(**raw_user)
+        return None
+
+    async def get_all(self) -> List[UserInDB]:
+        raw_users = await self.storage.get_all()
+        return [UserInDB(**row) for row in raw_users]
